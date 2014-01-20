24f6547a6004dbef82be9479f8dc6f354e577e04
Oat Liewsrisuk <oat@Oats-MacBook-Pro.local>
2014-01-20 15:58:12 -0800

    HW1

---
+++ HW1/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+	<head>
+	    <meta charset="UTF-8">
+	    <meta name="description" content="NBA Stars Page">
+	    <meta name="author" content="iphong">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	    <link rel="shortcut icon" href="img/logo.png">
+	    <title>NBA Stars</title>
+	    
+	    <!-- hot link to Bootstrap stylesheet -->
+	    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+
+	    <!-- our own stylesheet for overrides -->
+	    <link rel="stylesheet" href="css/styles.css">
+	</head>	
+	<body> 	
+	
+	<div class="jumbotron">
+	  <h1>NBA search</h1>
+
+	  <p><a class="btn btn-primary btn-lg" role="button">Search</a></p>
+	</div>
+		
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+         <!-- auto-play the carousel -->
+	    <script>
+	        $('.carousel').carousel();
+	    </script>
+
+
+	</body>
+</html>
